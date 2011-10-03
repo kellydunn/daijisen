73573ea94828974ae67d3622ace1f027a474eabc
kellydunn <defaultstring@gmail.com>
2011-10-02 17:07:12 -0700

    [lib] A little more refactoring

--- lib/daijisen.rb
+++ lib/daijisen.rb
@@ -1,22 +1,13 @@
+# TODO determine if I need these.
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-=begin
-Yahoo Daijisen Japanese Dictionary Scraper
-Author:  Kelly Dunn
-=end
-
 module Daijisen
-  VERSION = '0.0.5'
   require 'rubygems'
   require 'nokogiri'
   require 'open-uri'
   require 'cgi'
 
-  # Query Object.
-  # Effectively scrapes The Yahoo Daijisen Dictionary
-  # And finds definitions of the Japanese String passed in
-  #
   # TODO: Incorporate SHIFT_JS encoding.  Only UTF-8 works for now.
   class Query
     attr_accessor :defs, :query
@@ -27,7 +18,6 @@ module Daijisen
       get_raw_html()
     end
     
-    # Scraping function.
     def get_raw_html()
       url = "http://dic.yahoo.co.jp/search?stype=0&ei=UTF-8&dtype=2&p=" + CGI::escape(@query)
       html = Nokogiri::HTML(open(url))
@@ -41,11 +31,8 @@ module Daijisen
         yield d
       end
     end
-
-    private :get_raw_html
   end
 
-  # For delicious Ruby Modularity, Definitions will be OOPified.
   class Definition
     attr_accessor :link, :example, :reading
 
